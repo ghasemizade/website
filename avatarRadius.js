dee65f2cb37e52de29ee3d276ff6016d14db31ca
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-06-25 21:20:10 +0330

    update website

---
+++ avatarRadius.js
@@ -0,0 +1,15 @@
+const avatar = document.querySelector('.avatar')
+
+
+
+function changeRadius() {
+    avatar.style.borderTopRightRadius = Math.floor(Math.random() * 100) + "%";
+    avatar.style.borderTopLeftRadius = Math.floor(Math.random() * 100) + "%";
+    avatar.style.borderBottomRightRadius = Math.floor(Math.random() * 100) + "%";
+    avatar.style.borderBottomLeftRadius = Math.floor(Math.random() * 100) + "%";
+
+    setTimeout(changeRadius, 2000);
+}
+
+
+changeRadius();
