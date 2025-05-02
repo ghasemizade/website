030c299cf466f2883a7a14242ffa1c48129e86e9
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2025-05-02 22:46:27 +0330

    add route

---
+++ app/blogs/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BlogsPage = () => {
+    return (
+        <div>
+            <h1>Blogs</h1>
+            <p>Welcome to the blogs page!</p>
+        </div>
+    );
+};
+
+export default BlogsPage;
